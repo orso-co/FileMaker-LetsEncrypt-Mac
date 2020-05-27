6ce66383d2e6bf0d43227af34bee1b755b7cda43
Holger Frey <mail@holgerfrey.de>
2020-05-27 20:01:58 +0200

    changed settings for our instasllation

--- GetSSL.sh
+++ GetSSL.sh
@@ -5,43 +5,48 @@
 # Date: 5/7/2019
 # Version: 0.4
 
-# Need help? We can set this up to run on your server for you! Send an email to 
+# Need help? We can set this up to run on your server for you! Send an email to
 # contact@bluefeathergroup.com or give a call at (770) 765-6258
 
 # Change the domain variable to the domain/subdomain for which you would like
 # an SSL Certificate
-DOMAIN="fms.mycompany.com"
+DOMAIN_1="os.orso.co"
+DOMAIN_2="me.orso.co"
+DOMAIN_3="arpa.orso.co"
 
 # Change the contact email address to your real email address so that Let's Encrypt
 # can contact you if there are any problems #>
-EMAIL="myemail@mycompoany.com"
+EMAIL="it@orso.co"
 
-# Enter the path to your FileMaker Server directory, ending in a slash 
-SERVER_PATH="/Library/FileMaker Server/"
+# Enter the path to the configuration directory for Let's Encrypt
+LE_CONFIG_DIR="/usr/local/etc/letsencrypt"
+
+# Enter the path to your FileMaker Server directory, ending in a slash
+SERVER_PATH="/Library/FileMaker Server"
 
 #
 # --- you shouldn't need to edit anything below this line
 #
 
-WEB_ROOT="${SERVER_PATH}HTTPServer/htdocs"
+WEB_ROOT="${SERVER_PATH}/HTTPServer/htdocs"
 
 
 # Get the certificate
-certbot certonly --webroot -w "$WEB_ROOT" -d $DOMAIN --agree-tos -m "$EMAIL" --preferred-challenges "http" -n
+certbot certonly --test-cert --webroot -w "$WEB_ROOT" -d $DOMAIN_1 -d $DOMAIN_2 -d $DOMAIN_3 --agree-tos -m "$EMAIL" --preferred-challenges "http" -n --config-dir $LE_CONFIG_DIR
 
-cp "/etc/letsencrypt/live/${DOMAIN}/fullchain.pem" "${SERVER_PATH}CStore/fullchain.pem"
-cp "/etc/letsencrypt/live/${DOMAIN}/privkey.pem" "${SERVER_PATH}CStore/privkey.pem"
+cp "{$LE_CONFIG_DIR}/live/${DOMAIN}/fullchain.pem" "${SERVER_PATH}/CStore/fullchain.pem"
+cp "{$LE_CONFIG_DIR}/live/${DOMAIN}/privkey.pem" "${SERVER_PATH}/CStore/privkey.pem"
 
-chmod 640 "${SERVER_PATH}CStore/privkey.pem"
+chmod 640 "${SERVER_PATH}/CStore/privkey.pem"
 
 # Move an old certificate, if there is one, to prevent an error
-mv "${SERVER_PATH}CStore/serverKey.pem" "${SERVER_PATH}CStore/serverKey-old.pem"
+mv "${SERVER_PATH}/CStore/serverKey.pem" "${SERVER_PATH}/CStore/serverKey-old.pem"
 
 # Remove the old certificate
 fmsadmin certificate delete
 
 # Install the certificate
-fmsadmin certificate import "${SERVER_PATH}CStore/fullchain.pem" --keyfile "${SERVER_PATH}CStore/privkey.pem" -y
+fmsadmin certificate import "${SERVER_PATH}/CStore/fullchain.pem" --keyfile "${SERVER_PATH}/CStore/privkey.pem" -y
 
 # Stop FileMaker Server
 launchctl stop com.filemaker.fms
