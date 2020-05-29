988a6f5dc6bb798107470878fe841509d2f5fca8
Holger Frey <mail@holgerfrey.de>
2020-05-29 18:17:34 +0200

    full test script, second try

--- test.sh
+++ test.sh
@@ -19,6 +19,31 @@ EMAIL="it@orso.co"
 # Enter the path to your FileMaker Server directory, ending in a slash
 SERVER_PATH="/Library/FileMaker Server"
 
+# Enter the path to the Let's Encrypt config and key directory
+LE_CONFIG_DIR="/etc/letsencrypt"
 
 # Get the certificate
 certbot certonly --test-cert --standalone -d $DOMAIN --agree-tos -m "$EMAIL" --preferred-challenges "http" -n
+
+cp "{$LE_CONFIG_DIR}/live/${DOMAIN}/fullchain.pem" "${SERVER_PATH}/CStore/fullchain.pem"
+cp "{$LE_CONFIG_DIR}/live/${DOMAIN}/privkey.pem" "${SERVER_PATH}/CStore/privkey.pem"
+
+chmod 640 "${SERVER_PATH}/CStore/privkey.pem"
+
+# Move an old certificate, if there is one, to prevent an error
+mv "${SERVER_PATH}/CStore/serverKey.pem" "${SERVER_PATH}/CStore/serverKey-old.pem"
+
+# Remove the old certificate
+fmsadmin certificate delete
+
+# Install the certificate
+fmsadmin certificate import "${SERVER_PATH}/CStore/fullchain.pem" --keyfile "${SERVER_PATH}/CStore/privkey.pem" -y
+
+# Stop FileMaker Server
+launchctl stop com.filemaker.fms
+
+# Wait 15 seconds for it to stop
+sleep 15s
+
+# Start FileMaker Server again
+launchctl start com.filemaker.fms
